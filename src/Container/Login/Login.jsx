5361f943a20d1297d7ca4210cc38e4ace7832ace
Ariel_Santangelo <78393362+ariel3259@users.noreply.github.com>
2022-06-30 12:04:11 -0300

    Login view finished

--- src/Container/Login/Login.jsx
+++ src/Container/Login/Login.jsx
@@ -3,6 +3,7 @@ import { UsersReducer, initialState } from "../../Reducers/usersReducers";
 import {GridContainer} from "../../Components/GridContainer/GridContainer";
 import * as userActions from "../../Actions/UsersAction";
 import "./login.css";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
 
@@ -19,7 +20,31 @@ export const Login = () => {
                 height={100}
                 bg="#01386C">
                     <div className="login-container">
-
+                        <div className="field">
+                            <h1>Log in</h1>
+                        </div>
+                        <div className="field">
+                            <label>Username</label>
+                            <input 
+                                type="text" 
+                                className="username-login"
+                                onChange={onChangeUsername} />
+                        </div>
+                        <div className="field">
+                            <label>Password</label>
+                            <input 
+                                type="text"
+                                className="password-login"
+                                onChange={onChangePassword} />
+                        </div>
+                        <div className="field">
+                            <a className="btn-login">
+                                Login
+                            </a>
+                            <Link to="/register" className="btn-register">
+                                Register
+                            </Link>
+                        </div>
                     </div>
             </GridContainer>
         </>
